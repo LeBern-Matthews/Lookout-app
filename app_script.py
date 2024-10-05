e51584915fea451d128225b3fe9433158a0a5cba
LeBern-Matthews <128014055+LeBern-Matthews@users.noreply.github.com>
2024-10-05 12:16:43 -0400

    initialising the app class

--- app_script.py
+++ app_script.py
@@ -1,3 +1,15 @@
 import tkinter as tk
 
+#main app class
 
+class LookOutApp():
+    def __init__(self, root) -> None:
+        self.root=root
+        self.root.geography="500X500"
+        self.root.title=""
+        
+
+
+root=tk.Tk()
+App=LookOutApp(root)
+root.mainloop()
