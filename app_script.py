10ff7e5785a97ecb751ab0c6d2e8c942867aec19
LeBern-Matthews <128014055+LeBern-Matthews@users.noreply.github.com>
2024-11-27 08:55:34 -0400

    prev commit msg

--- app_script.py
+++ app_script.py
@@ -18,16 +18,16 @@ def main():
     #getcountry(IP)
     layout()
    
-    #root.resizable(False, False)
+    root.resizable(False, False)
     root.mainloop()
     
 def layout():
     paddingy=24
     navbar=tk.Frame(root, bg=BACKGROUND, height=40, borderwidth=5, border=5, width=360)
-    home_btn=tk.Button(navbar,image=home_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=1)
-    Checklist_btn=tk.Button(navbar,image=Checklist_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=1)
-    contacts_btn=tk.Button(navbar,image=contacts_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=1)
-    user_btn=tk.Button(navbar,image=user_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=1)
+    home_btn=tk.Button(navbar,image=home_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=0)
+    Checklist_btn=tk.Button(navbar,image=Checklist_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=0)
+    contacts_btn=tk.Button(navbar,image=contacts_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=0)
+    user_btn=tk.Button(navbar,image=user_icon,bg=BACKGROUND, activebackground=BACKGROUND, relief="flat", bd=0)
        
     home_btn.pack(side="left", padx=20, pady=paddingy)
     Checklist_btn.pack(side="left", padx=30, pady=paddingy)
